3587d3cf8cd63f6e7c818b647b1390e34ea13f5a
Sergei Sonin <sonin.sm@gmail.com>
2024-05-09 00:25:19 +0100

    Add simple echo

--- src/ssonin/memcached/core.clj
+++ src/ssonin/memcached/core.clj
@@ -1,7 +1,20 @@
 (ns ssonin.memcached.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:import [java.net ServerSocket]))
+
+(declare echo-client)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [socket (ServerSocket. 11211)]
+    (echo-client (.accept socket))))
+
+(defn- echo-client
+  [client-socket]
+  (let [in (io/reader client-socket)
+        out (io/writer client-socket)]
+    (while true
+      (doseq [line (line-seq in)]
+        (.write out line)
+        (.flush out)))))
