2d88d0d7db53fddd8376ed2368af00b4d87a28d7
Sergei Sonin <sonin.sm@gmail.com>
2024-11-16 23:40:04 +0000

    Handle multiple clients

--- src/ssonin/memcached/core.clj
+++ src/ssonin/memcached/core.clj
@@ -1,20 +1,34 @@
 (ns ssonin.memcached.core
   (:gen-class)
   (:require [clojure.java.io :as io])
-  (:import [java.net ServerSocket]))
+  (:import [java.net ServerSocket SocketException]))
 
-(declare echo-client)
+(defn- echo-client
+  [client-socket]
+  (try
+    (with-open [in (io/reader client-socket)
+                out (io/writer client-socket)]
+      (doseq [line (line-seq in)]
+        (.write out line)
+        (.flush out))
+      (println "Client disconnected"))
+    (catch SocketException e
+      (.println System/err (str "Client disconnected abruptly: " (.getMessage e))))))
 
 (defn -main
   [& args]
   (let [socket (ServerSocket. 11211)]
-    (echo-client (.accept socket))))
-
-(defn- echo-client
-  [client-socket]
-  (let [in (io/reader client-socket)
-        out (io/writer client-socket)]
+    (println "Started server on port 11211")
     (while true
-      (doseq [line (line-seq in)]
-        (.write out line)
-        (.flush out)))))
+      (try
+        (let [client-socket (.accept socket)]
+          (println "New client connected")
+          (future
+            (try
+              (with-open [client-socket client-socket]
+                (echo-client client-socket))
+              (catch Exception e
+                (.println System/err
+                          (str "Unexpected error handling client: " (.getMessage e)))))))
+        (catch Exception e
+          (.println System/err (str "Server error: " (.getMessage e))))))))
